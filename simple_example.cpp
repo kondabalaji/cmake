f031ffd4c4418a209450d91e35301e7a58ee0671
Konda Balaji <balajikonda02@gmail.com>
2025-02-09 14:09:57 +0000

    cmake

--- simple_example.cpp
+++ simple_example.cpp
@@ -6,7 +6,7 @@
 
 int main() {
 
-    std::cout << "Simple example C++ compiled correctly and ran." << std::endl;
+    std::cout << "Simple example C++ compiled correctly and ran cmake." << std::endl;
     std::cout << simple_lib_function() << std::endl;
 
     return 0;
